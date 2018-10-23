66d18f4a1825806a5ca4adff1599458fce2b1545
FalconTube <yannic.falke@gmail.com>
2018-10-23 13:40:33 +0200

    Added fortran files for github

---
+++ fortran_routines/bspline_kinds_module.f90
@@ -0,0 +1,20 @@
+!*****************************************************************************************
+!> author: Jacob Williams
+!  license: BSD
+!
+!### Description
+!  Numeric kind definitions for BSpline-Fortran.
+
+    module bspline_kinds_module
+
+    use,intrinsic :: iso_fortran_env
+
+    implicit none
+
+    private
+
+    integer,parameter,public :: wp = real64  !! Real precision
+
+!*****************************************************************************************
+    end module bspline_kinds_module
+!*****************************************************************************************
