d525c1b85cfe8e3c67b2a8bd2fa485fb6aee5a39
FalconTube <yannic.falke@gmail.com>
2019-01-14 12:05:25 +0100

    added rename.py

---
+++ rename_version.py
@@ -0,0 +1,11 @@
+import os
+import glob
+import sys
+
+py_vers = sys.version
+wheel_vers = 'py3' + str(py_vers.split('.')[1])
+print(wheel_vers)
+for file in glob.glob('dist/*.whl'):
+    out = file.replace('py3', wheel_vers)
+    os.rename(file, out)
+    print('Renamed {} to {}'.format(file, out))
