5eb62f807d08bf6c3085a8e79cc57072a3c2360d
FalconTube <yannic.falke@gmail.com>
2019-01-14 13:45:33 +0100

    fixed renaming

--- rename_version.py
+++ rename_version.py
@@ -6,6 +6,6 @@ py_vers = sys.version
 wheel_vers = 'py3' + str(py_vers.split('.')[1])
 print(wheel_vers)
 for file in glob.glob('dist/*.whl'):
-    out = file.replace('py3', wheel_vers)
+    out = file.replace('cp3', wheel_vers)
     os.rename(file, out)
     print('Renamed {} to {}'.format(file, out))
