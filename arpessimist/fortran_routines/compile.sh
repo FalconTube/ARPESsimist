a366ecf4718ad012011f92132ec5b981ca43afbf
FalconTube <yannic.falke@gmail.com>
2019-01-14 16:34:32 +0100

    appveyor

--- arpessimist/fortran_routines/compile.sh
+++ arpessimist/fortran_routines/compile.sh
@@ -3,7 +3,6 @@ gfortran -fPIC -c bspline_sub_module.f90
 gfortran -fPIC -c bspline_oo_module.f90
 gfortran -fPIC -c bspline_module.f90
 gfortran -c kmaps.f90
-#python3 -m numpy.f2py -m kmaps --f90flags='-fopenmp' -lgomp -I bspline_sub_module.o -I bspline_kinds_module.o -c kmaps.f90
 python3 -m numpy.f2py -m kmaps --f90flags='-fopenmp' -lgomp -I bspline_sub_module.o -I bspline_kinds_module.o -c kmaps.f90
 mv *.so ../src
 #python3 -m numpy.f2py -c -m kmaps -I bspline_sub_module.o -I bspline_kinds_module.o kmaps.f90
