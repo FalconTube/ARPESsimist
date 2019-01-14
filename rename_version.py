538849a96bc8e9683e677c634b5c7a243f0ab6aa
FalconTube <yannic.falke@gmail.com>
2019-01-14 14:29:48 +0100

    changed libgfortran

--- rename_version.py
+++ rename_version.py
@@ -3,9 +3,9 @@ import glob
 import sys
 
 py_vers = sys.version
-wheel_vers = 'py3' + str(py_vers.split('.')[1])
+wheel_vers = 'cp3' + str(py_vers.split('.')[1])
 print(wheel_vers)
 for file in glob.glob('dist/*.whl'):
-    out = file.replace('cp3', wheel_vers)
+    out = file.replace('py3', wheel_vers)
     os.rename(file, out)
     print('Renamed {} to {}'.format(file, out))
