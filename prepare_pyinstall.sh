f98473c859ddf29fae4d8816ce7111b445623dd6
FalconTube <yannic.falke@gmail.com>
2019-01-16 16:58:04 +0100

    added pypiwin32

--- prepare_pyinstall.sh
+++ prepare_pyinstall.sh
@@ -1,5 +1,6 @@
 rm -rf pyinst_dir
 pip install --quiet -U PyInstaller
+pip install --quiet -U pypiwin32
 mkdir pyinst_dir
 cp -r arpessimist/src/* pyinst_dir
 cd pyinst_dir
@@ -8,6 +9,7 @@ cp ../arpessimist/main_gui.py .
 sed -i 's/from .src./from /' main_gui.py
 sed -i "s/def run():/if __name__ == '__main__':/" main_gui.py
 pyi-grab_version cmd.exe arpessimst_version.txt
+du -sh arpessimst_version.txt
 #pyinstaller main_gui.py --windowed --onefile --version-file=arpessimst_version.txt --hidden-import=pywt._extensions._cwt
 pyinstaller main_gui.py --debug --onefile --name=ARPESsimist --version-file=arpessimst_version.txt --hidden-import=pywt._extensions._cwt
 cd dist
