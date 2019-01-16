64bb4a7b4c8824d8ce53e14b60911b26e49155fc
FalconTube <yannic.falke@gmail.com>
2019-01-16 17:19:00 +0100

    trying to restore working version of win compilation

--- prepare_pyinstall.sh
+++ prepare_pyinstall.sh
@@ -10,8 +10,8 @@ sed -i 's/from .src./from /' main_gui.py
 sed -i "s/def run():/if __name__ == '__main__':/" main_gui.py
 pyi-grab_version cmd.exe arpessimst_version.txt
 du -sh arpessimst_version.txt
-#pyinstaller main_gui.py --windowed --onefile --version-file=arpessimst_version.txt --hidden-import=pywt._extensions._cwt
-pyinstaller main_gui.py --debug --onefile --name=ARPESsimist --version-file=arpessimst_version.txt --hidden-import=pywt._extensions._cwt
+-pyinstaller main_gui.py --windowed --onefile --version-file=arpessimst_version.txt --hidden-import=pywt._extensions._cwt
+#pyinstaller main_gui.py --debug --onefile --name=ARPESsimist --version-file=arpessimst_version.txt --hidden-import=pywt._extensions._cwt
 cd dist
 echo "ls of dist in pyinstall"
 ls
