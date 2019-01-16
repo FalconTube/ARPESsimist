bdf22d333d5e19a3b8a3822f9c1a19ef494202cc
FalconTube <yannic.falke@gmail.com>
2019-01-16 12:31:27 +0100

    added version stuff for pyinstaller on windows

--- prepare_pyinstall.sh
+++ prepare_pyinstall.sh
@@ -1,5 +1,6 @@
 rm -rf pyinst_dir
 pip install --quiet -U PyInstaller
+pip install -U pypiwin32
 mkdir pyinst_dir
 cp -r arpessimist/src/* pyinst_dir
 cd pyinst_dir
@@ -11,4 +12,10 @@ pyinstaller main_gui.py --windowed --onefile --hidden-import=pywt._extensions._c
 cd dist
 echo "ls of dist in pyinstall"
 ls
+pyi-grab_version ARPESsimist.exe 
+pyi-grab_version ARPESsimist.exe > arpessimst_version.txt
+mv arpessimst_version.txt ..
+cd ..
+pyinstaller main_gui.py --windowed --onefile --version-file=arpessimst_version.txt --hidden-import=pywt._extensions._cwt
+cd dist
 mv main_gui* ../../dist/ARPESsimist.exe
