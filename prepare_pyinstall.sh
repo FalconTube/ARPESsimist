d3948f85151023e244edfd8c76129914f1ee0ae2
FalconTube <yannic.falke@gmail.com>
2019-01-16 13:03:16 +0100

    changes to pyinstaller

--- prepare_pyinstall.sh
+++ prepare_pyinstall.sh
@@ -8,14 +8,9 @@ sed -i 's/from \./from /' *.py
 cp ../arpessimist/main_gui.py .
 sed -i 's/from .src./from /' main_gui.py
 sed -i "s/def run():/if __name__ == '__main__':/" main_gui.py
-pyinstaller main_gui.py --windowed --onefile --hidden-import=pywt._extensions._cwt
+pyi-grab_version cmd.exe arpessimst_version.txt
+pyinstaller main_gui.py --windowed --onefile --version-file=arpessimst_version.txt --hidden-import=pywt._extensions._cwt
 cd dist
 echo "ls of dist in pyinstall"
 ls
-pyi-grab_version ARPESsimist.exe 
-pyi-grab_version ARPESsimist.exe > arpessimst_version.txt
-mv arpessimst_version.txt ..
-cd ..
-pyinstaller main_gui.py --windowed --onefile --version-file=arpessimst_version.txt --hidden-import=pywt._extensions._cwt
-cd dist
 mv main_gui* ../../dist/ARPESsimist.exe
