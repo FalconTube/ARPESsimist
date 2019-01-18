623aadadd2c4c38a3276a5663b2881f994a5132e
FalconTube <yannic.falke@gmail.com>
2019-01-18 15:56:51 +0100

    changed back f2py compiler

--- arpessimist/fortran_routines/compile_windows.sh
+++ arpessimist/fortran_routines/compile_windows.sh
@@ -3,7 +3,7 @@ gfortran -fPIC -c bspline_sub_module.f90
 gfortran -fPIC -c bspline_oo_module.f90
 gfortran -fPIC -c bspline_module.f90
 gfortran -c kmaps.f90
-python -m numpy.f2py --compiler=msvc -m kmaps --f90flags='-fopenmp' -lgomp -I bspline_sub_module.o -I bspline_kinds_module.o -c kmaps.f90
+python -m numpy.f2py --compiler=mingw32 -m kmaps --f90flags='-fopenmp' -lgomp -I bspline_sub_module.o -I bspline_kinds_module.o -c kmaps.f90
 cp *.pyd ../src
 #cp *.pyd ../../dist
 #python3 -m numpy.f2py -c -m kmaps -I bspline_sub_module.o -I bspline_kinds_module.o kmaps.f90
