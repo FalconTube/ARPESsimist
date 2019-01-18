efc9d5e3e1926325290a94a59ae55d9cc672fd7b
FalconTube <yannic.falke@gmail.com>
2019-01-18 16:34:06 +0100

    added build directory for fortran

--- arpessimist/fortran_routines/compile.sh
+++ arpessimist/fortran_routines/compile.sh
@@ -3,7 +3,7 @@ gfortran -fPIC -c bspline_sub_module.f90
 gfortran -fPIC -c bspline_oo_module.f90
 gfortran -fPIC -c bspline_module.f90
 gfortran -c kmaps.f90
-python3 -m numpy.f2py -m kmaps --f90flags='-fopenmp' -lgomp -I bspline_sub_module.o -I bspline_kinds_module.o -c kmaps.f90
+python3 -m numpy.f2py -m kmaps --build-dir fortran_build --f90flags='-fopenmp' -lgomp -I bspline_sub_module.o -I bspline_kinds_module.o -c kmaps.f90
 #mv *.so ../src
 #python3 -m numpy.f2py -c -m kmaps -I bspline_sub_module.o -I bspline_kinds_module.o kmaps.f90
 # f2py3 -c bspline_module.f90 kmaps.f90 -m kmaps
