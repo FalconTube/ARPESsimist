dff1a81c8494fd977b77cd8d091b8baa6f8d5daa
FalconTube <yannic.falke@gmail.com>
2019-01-18 17:21:12 +0100

    small fix

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ from io import open
 import sys
 
 if 'win' in sys.platform:
-    extension_packages = {'arpessimist': ['src/*.pyd', 'src/images/*', 'fortran_routines/*', 'fortran_routines/fortran_build/']}
+    extension_packages = {'arpessimist': ['src/*.pyd', 'src/images/*', 'fortran_routines/*']}#, 'fortran_routines/fortran_build/']}
 else:
     extension_packages = {'arpessimist': ['src/*.so', 'src/images/*']}
 
