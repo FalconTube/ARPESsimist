c3aa5e472591a71909d9e9f7dbbe57cd5850adfc
FalconTube <yannic.falke@gmail.com>
2019-01-18 14:57:29 +0100

    implemented c++ again

--- arpessimist/fortran_routines/compile_windows.sh
+++ arpessimist/fortran_routines/compile_windows.sh
@@ -4,6 +4,7 @@ gfortran -fPIC -c bspline_oo_module.f90
 gfortran -fPIC -c bspline_module.f90
 gfortran -c kmaps.f90
 python -m numpy.f2py --compiler=mingw32 -m kmaps --f90flags='-fopenmp' -lgomp -I bspline_sub_module.o -I bspline_kinds_module.o -c kmaps.f90
-mv *.pyd ../src
+cp *.pyd ../src
+#cp *.pyd ../../dist
 #python3 -m numpy.f2py -c -m kmaps -I bspline_sub_module.o -I bspline_kinds_module.o kmaps.f90
 # f2py3 -c bspline_module.f90 kmaps.f90 -m kmaps
