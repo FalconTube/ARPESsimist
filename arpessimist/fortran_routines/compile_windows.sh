08437e198f94bf2ff47fe936d0764b715574a6c5
FalconTube <yannic.falke@gmail.com>
2019-01-18 15:26:41 +0100

    added static libraries to gfortran

--- arpessimist/fortran_routines/compile_windows.sh
+++ arpessimist/fortran_routines/compile_windows.sh
@@ -1,8 +1,8 @@
-gfortran -fPIC -c bspline_kinds_module.f90
-gfortran -fPIC -c bspline_sub_module.f90
-gfortran -fPIC -c bspline_oo_module.f90
-gfortran -fPIC -c bspline_module.f90
-gfortran -c kmaps.f90
+gfortran -static-libgcc -static-libgfortran fPIC -c bspline_kinds_module.f90
+gfortran -static-libgcc -static-libgfortran fPIC -c bspline_sub_module.f90
+gfortran -static-libgcc -static-libgfortran fPIC -c bspline_oo_module.f90
+gfortran -static-libgcc -static-libgfortran fPIC -c bspline_module.f90
+gfortran -static-libgcc -static-libgfortran -c kmaps.f90
 python -m numpy.f2py --compiler=mingw32 -m kmaps --f90flags='-fopenmp' -lgomp -I bspline_sub_module.o -I bspline_kinds_module.o -c kmaps.f90
 cp *.pyd ../src
 #cp *.pyd ../../dist
