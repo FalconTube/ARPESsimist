bbb8daaa45b8fd31b8150bf637d27d428bb84b84
FalconTube <yannic.falke@gmail.com>
2019-01-20 17:04:30 +0100

    trying to link libraries

--- arpessimist/fortran_routines/compile.sh
+++ arpessimist/fortran_routines/compile.sh
@@ -4,7 +4,7 @@ gfortran -static -fPIC -c bspline_oo_module.f90
 gfortran -static -fPIC -c bspline_module.f90
 gfortran -static -c kmaps.f90
 #python3 -m numpy.f2py -m kmaps --build-dir fortran_build --f90flags='-fopenmp' -lgomp -I bspline_sub_module.o -I bspline_kinds_module.o -c kmaps.f90
-python3 -m numpy.f2py -m kmaps --f90flags='-fopenmp' -lgomp  bspline_sub_module.o  bspline_kinds_module.o -c kmaps.f90
+python3 -m numpy.f2py -m kmaps --f90flags='-fopenmp' -lgfortran -lgcc -lgomp  bspline_sub_module.o  bspline_kinds_module.o -c kmaps.f90
 #mv *.so ../src
 #python3 -m numpy.f2py -c -m kmaps -I bspline_sub_module.o -I bspline_kinds_module.o kmaps.f90
 # f2py3 -c bspline_module.f90 kmaps.f90 -m kmaps
