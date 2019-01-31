6cda81d6cdd7275d9f161ba3a32dba9e36737b5e
FalconTube <yannic.falke@gmail.com>
2019-01-31 12:05:58 +0100

    fixed map parameters

--- arpessimist/src/ask_map_parameters.py
+++ arpessimist/src/ask_map_parameters.py
@@ -12,6 +12,7 @@ class MapParameterBox(QDialog):
     """ Initiates Box for user input of Map parameters """
 
     def __init__(self, saved_values=None, pol_available=False):
+        print('invals {}'.format(saved_values))
         super().__init__()
         self.pol_available = pol_available
         self.saved_values = saved_values
@@ -35,7 +36,6 @@ class MapParameterBox(QDialog):
             # Define LineEdit Fields
             ksteps = QLineEdit("0.01", self)
             esteps = QLineEdit("0.005", self)
-            #pol_off = QLineEdit("0.0", self)
             tilt = QLineEdit("0.0", self)
             angle_off = QLineEdit("0.0", self)
             azi = QLineEdit("0.0", self)
@@ -48,7 +48,6 @@ class MapParameterBox(QDialog):
             # how to do better...
             ksteps = QLineEdit(str(self.saved_values[0]), self)
             esteps = QLineEdit(str(self.saved_values[1]), self)
-            #pol_off = QLineEdit("0.0", self)
             tilt = QLineEdit(str(self.saved_values[2]), self)
             angle_off = QLineEdit(str(self.saved_values[3]), self)
             azi = QLineEdit(str(self.saved_values[4]), self)
@@ -73,26 +72,24 @@ class MapParameterBox(QDialog):
         # Add Edits
         self.lay.addWidget(ksteps, 0, 1)
         self.lay.addWidget(esteps, 1, 1)
-        #self.lay.addWidget(pol_off, 2, 1)
+        self.lay.addWidget(tilt, 2, 1)
         self.lay.addWidget(angle_off, 3, 1)
-        self.lay.addWidget(tilt, 4, 1)
-        self.lay.addWidget(azi, 5, 1)
-        self.lay.addWidget(kxmin, 6, 1)
-        self.lay.addWidget(kxmax, 7, 1)
-        self.lay.addWidget(kymin, 8, 1)
-        self.lay.addWidget(kymax, 9, 1)
+        self.lay.addWidget(azi, 4, 1)
+        self.lay.addWidget(kxmin, 5, 1)
+        self.lay.addWidget(kxmax, 6, 1)
+        self.lay.addWidget(kymin, 7, 1)
+        self.lay.addWidget(kymax, 8, 1)
 
         # Add Labels
         self.lay.addWidget(ksteps_label, 0, 0)
         self.lay.addWidget(esteps_label, 1, 0)
-        #self.lay.addWidget(pol_off_label, 2, 0)
-        self.lay.addWidget(angle_off_label, 3, 0)
-        self.lay.addWidget(tilt_label, 4, 0)
-        self.lay.addWidget(azi_label, 5, 0)
-        self.lay.addWidget(kxmin_label, 6, 0)
-        self.lay.addWidget(kxmax_label, 7, 0)
-        self.lay.addWidget(kymin_label, 8, 0)
-        self.lay.addWidget(kymax_label, 9, 0)
+        self.lay.addWidget(angle_off_label, 2, 0)
+        self.lay.addWidget(tilt_label, 3, 0)
+        self.lay.addWidget(azi_label, 4, 0)
+        self.lay.addWidget(kxmin_label, 5, 0)
+        self.lay.addWidget(kxmax_label, 6, 0)
+        self.lay.addWidget(kymin_label, 7, 0)
+        self.lay.addWidget(kymax_label, 8, 0)
 
         if not self.pol_available:
             if self.saved_values == None:
@@ -106,12 +103,12 @@ class MapParameterBox(QDialog):
             pol_max_label = QLabel("Azimuth End", self)
             # self.pol_steps_label = QLabel('Polar Stepsize', self)
 
-            self.lay.addWidget(pol_min, 10, 1)
-            self.lay.addWidget(pol_max, 11, 1)
+            self.lay.addWidget(pol_min, 9, 1)
+            self.lay.addWidget(pol_max, 10, 1)
             # self.lay.addWidget(self.pol_steps, 9, 1)
 
-            self.lay.addWidget(pol_min_label, 10, 0)
-            self.lay.addWidget(pol_max_label, 11, 0)
+            self.lay.addWidget(pol_min_label, 9, 0)
+            self.lay.addWidget(pol_max_label, 10, 0)
             # self.lay.addWidget(self.pol_steps_label, 9, 0)
 
         self.over_layout.addLayout(self.lay)
@@ -133,4 +130,5 @@ class MapParameterBox(QDialog):
             if "Line" in str(thiswidget):
                 outvals.append(float(thiswidget.text())) # save the values in order
             lastwidget = thiswidget
+        print('outvals {}'.format(outvals))
         return outvals
